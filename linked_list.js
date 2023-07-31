855409665005fcc3c7e55f63d66c4143b678c33c
PannKavyar <thiri3611@gmail.com>
2023-07-31 15:45:58 +0630

    LinkedList methods adding

--- linked_list.js
+++ linked_list.js
@@ -48,12 +48,30 @@ class LinkedList{
             node=node.next;
         }
     }
+
+    getAt(index){
+        if(!this.head){
+            return;
+        }
+        let counter=0;
+        let node=this.head;
+        while(node){
+            if(counter === index){
+                return node;
+            }
+            counter ++;
+            node=node.next;
+        }
+        return null;
+    }
+
     removeFirst(){
         if (!this.head){
             return;
         }
         return this.head=this.head.next;
     }
+
     removeLast(){
         if (!this.head){
             return;
@@ -82,6 +100,18 @@ class LinkedList{
             return this.head;
         }
     }
+    /*Creating Circular LinkedList*/
+    createCircular(index){
+         let lastNode=linkedList.getLast();
+         if(lastNode){
+             lastNode.next=this.getAt(index);
+             return lastNode;
+         }
+            return false;
+    }
+    clear(){
+       return this.head=null;
+    }
 
 }
 const linkedList=new LinkedList();
@@ -89,12 +119,65 @@ const node=new Node("Hey");
 linkedList.head=node;
 console.log(linkedList);
 linkedList.insertFirst("Hi");
+linkedList.insertFirst("Ok");
 linkedList.insertFirst("Huh");
+
+
+/*======Finding midpoint of the linked list========*/
+function getMidpoint(linkedList){
+    let slow=linkedList.getFirst();
+    let fast=linkedList.getFirst();
+    while(fast.next && fast.next.next){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return slow;
+}
+
+
+/*Checking the LinkedList is circular or not*/
+function checkCircular(linkedList){
+let slow=linkedList.getFirst();
+let fast=linkedList.getFirst();
+while(fast.next && fast.next.next){
+    slow=slow.next;
+    fast=fast.next.next;
+    if(slow === fast){
+        return true;
+    }
+}
+return false;
+}
+
+/*return the node which is n space from the last node*/
+function fromLastNode(linkedList,n){
+    let slow=linkedList.getFirst();
+    let fast=linkedList.getFirst();
+    for(let i=0; i<n; i++){
+      fast=fast.next;
+   }
+    while(fast.next){
+        fast=fast.next;
+        slow=slow.next;
+    }
+    return slow;
+}
+
+
 // console.log("Insert first data", linkedList);
 // console.log("LinkedList size is:",linkedList.size());
 // console.log("Get First Node",linkedList.getFirst());
 // console.log("Get Last Node", linkedList.getLast());
 // console.log("Remove First" , linkedList.removeFirst());
 // console.log("Remove Last",linkedList.removeLast());
-console.log("Insert Last data is",linkedList.insertLast("Yes"));
-console.log("Get Insert Last Data is",linkedList.getLast());
+// console.log("Insert Last data is",linkedList.insertLast("Yes"));
+// console.log("Get Insert Last Data is",linkedList.getLast());
+// linkedList.createCircular(1);
+// console.log("=============================================");
+// console.log("Get midpoint data is" , getMidpoint(linkedList));
+// console.log("=============================================");
+// console.log("Checking the LinledList is Circular" ,checkCircular(linkedList));
+// console.log("==============================================");
+// console.log("The node which is 3 space from the last node is", fromLastNode(linkedList,3));
+console.log(linkedList.clear());
+console.log(linkedList);
